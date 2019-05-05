468b6113402574fe52cbd7818b58a86a02a114d6
KDragon <elkarouani@gmail.com>
2019-05-05 15:23:13 +0100

    Task: using layouts in views

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,11 +1,5 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
-        hello from about page
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    hello from about page
+@endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,11 +1,5 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
-        Hello from the index view
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    Hello from the index view
+@endsection
